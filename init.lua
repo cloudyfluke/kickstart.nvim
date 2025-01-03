5d268998062f21ddb5f308cf18318827393c13c2
Seng Ye Xiong <cloudyfluke@gmail.com>
2025-01-03 21:33:12 +0000

    nice to haves

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -145,8 +145,12 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- Whitespace Display
+vim.cmd [[set listchars=space:·,tab:▷\ ,eol:↵,trail:•,extends:⟩,precedes:⟨]]
+vim.cmd [[set invlist]]
+vim.cmd [[set tabstop=4]]
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
@@ -615,17 +619,34 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        gopls = {
+          staticcheck = true,
+          filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+          gopls = {
+            gofumpt = true,
+            completeUnimported = true,
+            usePlaceholders = true,
+            staticcheck = true,
+            directoryFilters = { '-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules' },
+            semanticTokens = true,
+            analyses = {
+              unusedparams = true,
+            },
+          },
+        },
+        pyright = {},
         -- ts_ls = {},
-        --
+        bashls = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        -- gleam = {},
+        elixirls = {
+          filetypes = { 'elixir', 'exs', 'ex' },
+          staticcheck = true,
+          elixirls = {
+            cmd = { '/opt/elixir-ls/language_server.sh' },
+          },
+        },
 
         lua_ls = {
           -- cmd = { ... },
@@ -937,7 +958,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
